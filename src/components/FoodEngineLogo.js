56a747d70a0fa84d7521111f11528cdf60107fa3
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-10-03 01:06:51 +0800

    FoodEngine updates

--- src/components/FoodEngineLogo.js
+++ src/components/FoodEngineLogo.js
@@ -3,10 +3,44 @@ import React from "react";
 const FoodEngineLogo = () => {
     return (
         <>
-            <div>
-                <img src="/logo192.png" alt="bimbimbap" />
-                hello
+            <div className="FoodEngineWrapper">
+                <div className="image_wrapper">
+                    <img
+                        className="bibimbap"
+                        src="/bibimbap.png"
+                        alt="bimbimbap"
+                    />
+                </div>
+                <span className="title">THE FOOD ENGINE</span>
             </div>
+            <style jsx="true">
+                {`
+                    .FoodEngineWrapper {
+                        display: flex;
+                        flex-direction: column;
+                        width: 600px;
+                        justify-content: center;
+                        align-items: center;
+                        gap: 20px;
+                    }
+
+                    .image_wrapper {
+                        width: 100%;
+                        display: flex;
+                        justify-content: center;
+                    }
+
+                    .bibimbap {
+                        height: 400px;
+                        width: 400px;
+                    }
+
+                    .title {
+                        font-size: 32px;
+                        color: rgba(206, 145, 62, 0.94);
+                    }
+                `}
+            </style>
         </>
     );
 };
