e87686dab767a1531f947ca8478f3ebc9d62024b
Lee Bo Hua <bohua12@gmail.com>
2022-10-31 15:47:38 +0800

    NavLink from loginpage to search

--- src/components/FoodEngineLogo.js
+++ src/components/FoodEngineLogo.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 const FoodEngineLogo = () => {
     return (
         <React.Fragment>
             <div className="FoodEngineWrapper">
                 <div className="image_wrapper">
+                <NavLink className="nav-link"  to='/search'>                    
                     <img
-                        className="bibimbap"
-                        src="/bibimbap.png"
-                        alt="bimbimbap"
+                    className="bibimbap"
+                    src="/bibimbap.png"
+                    alt="bimbimbap"
                     />
+                </NavLink>
                 </div>
                 <span className="title">THE FOOD ENGINE</span>
             </div>
