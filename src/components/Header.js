cb3dd992775f3270325f26437ef21fa490942ebc
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-11-01 23:27:21 +0800

    Signout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,67 @@
-import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-import './header.css';
+import React, { Component } from "react";
+import {
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Collapse,
+    NavItem,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import "./header.css";
+import { auth } from "./../firebase";
+import { getAuth, signOut } from "firebase/auth";
 
+const handleSignOut = () => {
+    console.log("Clicked");
+    signOut(auth)
+        .then(() => {
+            // Sign-out successful.
+        })
+        .catch((error) => {
+            // An error happened.
+        });
+};
 
 class Header extends Component {
     constructor(props) {
         super(props);
-    
-
-      }
+    }
 
     render() {
-        return(
+        return (
             <div class="topnav container">
-<NavLink className="nav-link"  to='/events'><img src='/bibimbap.png' height="45" width="45" alt='userimg' /></NavLink>
-                <a class="appName" >FOOD ENGINE</a>
+                <NavLink className="nav-link" to="/events">
+                    <img
+                        src="/bibimbap.png"
+                        height="45"
+                        width="45"
+                        alt="userimg"
+                    />
+                </NavLink>
+                <a class="appName">FOOD ENGINE</a>
                 <div className="topnav-right">
-                    <NavLink className="nav-link"  to='/home'><img src='/user1.png' height="45" width="45" alt='userimg' /></NavLink>
-                    <NavLink className="nav-link" to='/search'><img src='/location1.png' height="40" width="40" alt='locationimg' /></NavLink>
+                    <NavLink className="nav-link" to="/home">
+                        <img
+                            src="/user1.png"
+                            height="45"
+                            width="45"
+                            alt="userimg"
+                        />
+                    </NavLink>
+                    <NavLink className="nav-link" to="/search">
+                        <img
+                            src="/location1.png"
+                            height="40"
+                            width="40"
+                            alt="locationimg"
+                        />
+                    </NavLink>
+                    <button onClick={handleSignOut}>Sign out button</button>
                 </div>
-                    
             </div>
         );
     }
 }
-
 
 export default Header;
